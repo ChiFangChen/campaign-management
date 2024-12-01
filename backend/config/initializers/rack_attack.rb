7b310c26a39ea543b913a1cc8160a065ca6151d3
Kiki Chen <kiki830514@gmail.com>
2024-12-01 15:56:42 +0800

    feat(backend): protect the server by restricting the access frequency

---
+++ backend/config/initializers/rack_attack.rb
@@ -0,0 +1,13 @@
+class Rack::Attack
+  throttle('req/ip', limit: 5, period: 1.minute) do |req|
+    req.ip
+  end
+
+  safelist("allow localhost") do |req|
+    ["127.0.0.1", "::1"].include?(req.ip)
+  end
+
+  self.throttled_response = ->(env) {
+    [ 429, { "Content-Type" => "text/plain" }, ["Rate limit exceeded. Try again later.\n"]]
+  }
+end
