d1d538e88666e4b27c1b1077e832381b3a2a9d8c
Kiki Chen <kiki830514@gmail.com>
2024-12-05 20:12:22 +0800

    feat(frontend): add virtual list to invoice detail page

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,13 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatch } from 'react-router-dom';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from './AppSidebar';
 
-export const Layout = () => (
-  <SidebarProvider>
-    <AppSidebar />
-    <main className="flex-1 relative px-16 py-4">
-      <SidebarTrigger className="absolute top-4 left-4" />
-      <Outlet />
-    </main>
-  </SidebarProvider>
-);
+export const Layout = () => {
+  const withoutPaddingYLayout = useMatch('/invoices/:id');
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className={`flex-1 relative px-16${withoutPaddingYLayout ? '' : ' py-4'}`}>
+        <SidebarTrigger className="absolute top-4 left-4" />
+        <Outlet />
+      </main>
+    </SidebarProvider>
+  );
+};
--- frontend/src/pages/Invoices/InvoiceDetail.tsx
+++ frontend/src/pages/Invoices/InvoiceDetail.tsx
@@ -1,4 +1,8 @@
+import { useRef } from 'react';
 import { useParams } from 'react-router-dom';
+import { useVirtualizer } from '@tanstack/react-virtual';
+import styled from 'styled-components';
+
 import { routes } from '@/routes';
 import { readableTime } from '@/lib/formatter-utils';
 import { useInvoiceDetail } from '@/queries/invoices';
@@ -6,6 +10,11 @@ import { Breadcrumb, Title, Skeleton } from '@/components';
 import { AmountCard } from './AmountCard';
 import { CampaignCard } from './CampaignCard';
 import { ExportButton } from './ExportButton';
+
+const ScrollArea = styled.div`
+  margin-right: -14px;
+  padding-right: 14px;
+`;
 
 const breadcrumbList = [
   {
@@ -16,10 +25,18 @@ const breadcrumbList = [
 
 export const InvoiceDetail = () => {
   const { id } = useParams();
+  const listRef = useRef<HTMLDivElement | null>(null);
   const { data, isLoading, isFetching } = useInvoiceDetail(id as string);
 
+  const rowVirtualizer = useVirtualizer({
+    count: data?.campaigns.length || 0,
+    getScrollElement: () => listRef.current,
+    estimateSize: () => 235,
+    measureElement: (el: HTMLDivElement) => el.offsetHeight,
+  });
+
   return (
-    <div>
+    <div className="flex flex-col pt-4" style={{ height: `calc(100vh - 1rem)` }}>
       <Breadcrumb list={breadcrumbList} />
       <div className="flex justify-between">
         <Title>
@@ -42,16 +59,23 @@ export const InvoiceDetail = () => {
         <AmountCard type="final" data={data} />
       </div>
 
-      <div>
-        {data?.campaigns.map((campaign) => (
-          <CampaignCard
-            key={campaign.id}
-            campaign={campaign}
-            isFetching={isFetching}
-            isLoading={isLoading}
-          />
-        ))}
-      </div>
+      <ScrollArea ref={listRef} className="flex-1 overflow-auto">
+        <div
+          className="relative flex flex-col gap-4 mx-2.5"
+          style={{
+            height: rowVirtualizer.getTotalSize(),
+          }}
+        >
+          {data?.campaigns.map((campaign) => (
+            <CampaignCard
+              key={campaign.id}
+              campaign={campaign}
+              isFetching={isFetching}
+              isLoading={isLoading}
+            />
+          ))}
+        </div>
+      </ScrollArea>
     </div>
   );
 };
