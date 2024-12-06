3934a367b0ac6298a4eeb80f1789bf717e768234
Kiki Chen <kiki830514@gmail.com>
2024-12-06 19:29:21 +0800

    feat(backend): generate test data for an invoice

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -3,36 +3,41 @@ require 'json'
 file_path = Rails.root.join('db', 'data', 'placements_teaser_data.json')
 data = JSON.parse(File.read(file_path))
 
-InvoiceLineItem.destroy_all
-Invoice.destroy_all
-LineItem.destroy_all
-Campaign.destroy_all
-
-data.each do |entry|
-  campaign = Campaign.find_or_create_by(
-    id: entry["campaign_id"],
-    name: entry["campaign_name"]
-  )
-
-  line_item = LineItem.find_or_create_by(
-    id: entry["id"],
-    name: entry["line_item_name"],
-    booked_amount: entry["booked_amount"],
-    actual_amount: entry["actual_amount"],
-    campaign: campaign
-  )
-
-  invoice = Invoice.create!(adjustments: entry["adjustments"])
-  invoice.line_items << line_item
-end
-
-# Create some data(Line Items in Campaign ID 3 & 4) to Invoice ID 7988
 ActiveRecord::Base.transaction do
-  target_invoice = Invoice.find(7988)
+  InvoiceLineItem.destroy_all
+  Invoice.destroy_all
+  LineItem.destroy_all
+  Campaign.destroy_all
 
-  line_items = LineItem.where(campaign_id: [3, 4])
+  data.each do |entry|
+    campaign = Campaign.find_or_create_by(
+      id: entry["campaign_id"],
+      name: entry["campaign_name"]
+    )
 
-  line_items.each do |line_item|
-    InvoiceLineItem.find_or_create_by(invoice: target_invoice, line_item: line_item)
+    line_item = LineItem.find_or_create_by(
+      id: entry["id"],
+      name: entry["line_item_name"],
+      booked_amount: entry["booked_amount"],
+      actual_amount: entry["actual_amount"],
+      campaign: campaign
+    )
+
+    invoice = Invoice.create!(adjustments: entry["adjustments"])
+    invoice.line_items << line_item
   end
+
+  # Create some data to an invoice
+  target_invoice = Invoice.first
+  campaigns = Campaign.limit(10)
+  line_items = LineItem.where(campaign_id: campaigns.pluck(:id))
+
+  existing_line_item_ids = InvoiceLineItem.where(invoice: target_invoice).pluck(:line_item_id)
+  line_items_to_add = line_items.where.not(id: existing_line_item_ids)
+
+  line_items_to_add.each do |line_item|
+    InvoiceLineItem.create!(invoice: target_invoice, line_item: line_item)
+  end
+
+  puts "Successfully associated #{line_items_to_add.count} line items from campaigns #{campaigns.pluck(:id)} to Invoice ##{target_invoice.id}"
 end
