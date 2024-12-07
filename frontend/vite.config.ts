188b07a97e059a4ea944c8f1715abd0edd075b69
Kiki Chen <kiki830514@gmail.com>
2024-12-07 19:04:50 +0800

    feat(frontend): add dependencies to run the test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { configDefaults } from 'vitest/config';
+import type { InlineConfig } from 'vitest';
 import path from 'path';
 
 // https://vite.dev/config/
@@ -10,4 +12,15 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests/setup.ts',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'json', 'lcov'],
+      exclude: ['node_modules/', 'dist/', '**/*.d.ts', '**/*.test.{ts,tsx}', '**/types/**'],
+    },
+  } satisfies InlineConfig,
 });
