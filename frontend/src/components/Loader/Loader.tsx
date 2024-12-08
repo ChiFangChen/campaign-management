a2453ed23553c70765428275cade2d8e097fc807
Kiki Chen <kiki830514@gmail.com>
2024-12-08 21:01:33 +0800

    fix(frontend): update the styles for loader

--- frontend/src/components/Loader/Loader.tsx
+++ frontend/src/components/Loader/Loader.tsx
@@ -18,21 +18,20 @@ const StyledLoader = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 1;
-  background-color: var(--tw-color-gray-50);
   opacity: 0.6;
 `;
 
 const LoaderIcon = styled.div`
   width: 40px;
   height: 40px;
-  border: 4px solid var(--tw-color-gray-200); /* 淺色邊框 */
-  border-top: 4px solid var(--tw-color-gray-300); /* 深色邊框 */
+  border-width: 4px;
+  border-style: solid;
   border-radius: 50%;
   animation: ${spin} 1s linear infinite;
 `;
 
 export const Loader = (props: HTMLAttributes<HTMLDivElement>) => (
-  <StyledLoader {...props}>
-    <LoaderIcon />
+  <StyledLoader className="bg-gray-50 dark:bg-neutral-950" {...props}>
+    <LoaderIcon className="border-gray-200 border-t-gray-300 dark:border-neutral-700 dark:border-t-neutral-800 " />
   </StyledLoader>
 );
